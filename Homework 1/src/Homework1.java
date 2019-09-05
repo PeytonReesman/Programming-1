74058e56b7155499dcde3b5935aebc17b08b1d96
peytonreesman <peytonreesman@10.242.2.35>
2019-09-05 12:15:43 -0400

    Homework done

---
+++ Homework 1/src/Homework1.java
@@ -0,0 +1,22 @@
+
+public class Homework1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		// Homework 1
+		// Name: Peyton Reesman
+		System.out.println("My To Do List:");
+		System.out.println("1. Wake up");
+		System.out.println("2. Mow the lawn");
+		System.out.println("3. Cut the bushes");
+		System.out.println("4. Do dishes");
+		System.out.println("5. Walk the dog");
+		System.out.println("6. Do laundry");
+		System.out.println("7. Wash the car");
+		System.out.println("8. Do homework");
+		System.out.println("9. Study");
+		System.out.println("10. Go to bed");
+	}
+
+}
