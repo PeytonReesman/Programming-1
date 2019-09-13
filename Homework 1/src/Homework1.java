87fef063a033557c540d2e6ada24a1933b79f3dd
peytonreesman <peytonreesman@10.0.0.2>
2019-09-12 22:33:50 -0400

    Ready for review.

--- Homework 1/src/Homework1.java
+++ Homework 1/src/Homework1.java
@@ -1,11 +1,15 @@
+import java.util.Scanner;
+
+//Project: Homework 1
+//Author: Peyton Reesman
+
 
 public class Homework1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		// Homework 1
-		// Name: Peyton Reesman
+		// Output my to do list
 		System.out.println("My To Do List:");
 		System.out.println("1. Wake up");
 		System.out.println("2. Mow the lawn");
@@ -17,6 +21,17 @@ public class Homework1 {
 		System.out.println("8. Do homework");
 		System.out.println("9. Study");
 		System.out.println("10. Go to bed");
+		System.out.println(" ");
+		
+		int age;
+		double myNumber = 4.577;
+		char middleInitial = 'D';
+		String myName = "Brad";
+		System.out.print("Hello, " +myName + " " +middleInitial + "!\nYour favorite number is " + myNumber + ".");
+
+		
+		
+		
 	}
 
 }
---
+++ Reesman Program 1/src/Program1.java
@@ -0,0 +1,29 @@
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//Project Name:   Program 1
+		//Description:    Build a program with three unique variables, print and println at least once, as well as one in-code comment.
+		//Last modified   11Sep2019
+		//Author:         Peyton A Reesman
+		
+		byte age = 21;
+		int favoriteNumber = 28;
+		char middleInitial = 'A';
+		String myName = "Peyton";
+		String myLastName = "Reesman";
+		
+		//Outputting my name and my age
+		System.out.println("Hello, my name is " +myName + " " + middleInitial + " " + myLastName + "," + " I am" + " " + age + " years old.");
+		
+		//Outputting my favorite number
+		System.out.print("My favorite number is" + " " + favoriteNumber + ".");
+		
+		
+		
+		
+	}
+
+}
